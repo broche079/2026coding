aa90016658aaacf91e9e64c81c5683561f6f6f95
broche079 <lary950709@gmail.com>
2026-04-23 15:41:09 +0800

    week09

---
+++ week09/week09-7.py
@@ -0,0 +1,20 @@
+#week09-7.py 學習計畫 Linked List 第4題
+#LeetCode 2130. Maximum Twin Sum of a Linked List
+#頭尾 兩兩配再一起 希望加起來最大
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        a = []
+        while head:
+            a.append( head.val )
+            head = head.next
+
+        N = len(a)
+        ans = 0
+        for i in range(N):
+            ans = max(ans, a[i]+a[N-1-i])
+        return ans
